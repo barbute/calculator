18471c5625351be85ef944824f9cfaf5a2736cea
barbute <jackhdemeterio@gmail.com>
2025-05-19 21:03:28 -0500

    Add four operations in app.js

--- app.js
+++ app.js
@@ -4,3 +4,19 @@
  * This file is part of etch_a_sketch and is licensed under the MIT License.
  * See the LICENSE file in the root of the project for more information.
  */
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
