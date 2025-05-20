eb10e9c8b7fbdba7e221d411daef1a79f6eeacbf
barbute <jackhdemeterio@gmail.com>
2025-05-19 21:23:42 -0500

    Add operate function to evaluate arguments

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
  * This file is part of etch_a_sketch and is licensed under the MIT License.
  * See the LICENSE file in the root of the project for more information.
  */
-
 function add(x, y) {
   return x + y;
 }
@@ -19,4 +18,39 @@ function multiply(x, y) {
 
 function divide(x, y) {
   return x / y;
+}
+
+const Operation = {
+  ADD: "add",
+  SUBTRACT: "subtract",
+  MULTIPLY: "multiply",
+  DIVIDE: "divide",
+  NOOP: "noop"
+}
+
+let argumentOne = 0;
+let argumentTwo = 0;
+let operation = Operation.NOOP;
+
+function operate(argOne, argTwo, op) {
+  let result = 0.0;
+  switch (op) {
+    case Operation.ADD:
+      result = add(argOne, argTwo);
+      break;
+    case Operation.SUBTRACT:
+      result = subtract(argOne, argTwo);
+      break;
+    case Operation.MULTIPLY:
+      result = multiply(argOne, argTwo);
+      break;
+    case Operation.DIVIDE:
+      result = divide(argOne, argTwo);
+      break;
+    default:
+      result = 0.0;
+      console.log("INVALID OPERATION");
+      break;
+  }
+  return result.toPrecision(3);
 }
