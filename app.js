f1594716ee18fb7c4905b62a5bb9803638dbad39
barbute <jackhdemeterio@gmail.com>
2025-05-22 17:54:53 -0500

    Display number on screen
    TODO Add argument switching functionality

--- app.js
+++ app.js
@@ -28,29 +28,66 @@ const Operation = {
   NOOP: "noop"
 }
 
-let argumentOne = 0;
-let argumentTwo = 0;
+let argumentOne = null;
+let argumentTwo = null;
 let operation = Operation.NOOP;
 
 function operate(argOne, argTwo, op) {
   let result = 0.0;
-  switch (op) {
-    case Operation.ADD:
-      result = add(argOne, argTwo);
-      break;
-    case Operation.SUBTRACT:
-      result = subtract(argOne, argTwo);
-      break;
-    case Operation.MULTIPLY:
-      result = multiply(argOne, argTwo);
-      break;
-    case Operation.DIVIDE:
-      result = divide(argOne, argTwo);
-      break;
-    default:
-      result = 0.0;
-      console.log("INVALID OPERATION");
-      break;
+  if (argOne != null && argTwo != null) {
+    switch (op) {
+      case Operation.ADD:
+        result = add(argOne, argTwo);
+        break;
+      case Operation.SUBTRACT:
+        result = subtract(argOne, argTwo);
+        break;
+      case Operation.MULTIPLY:
+        result = multiply(argOne, argTwo);
+        break;
+      case Operation.DIVIDE:
+        result = divide(argOne, argTwo);
+        break;
+      default:
+        result = 0.0;
+        console.log("INVALID OPERATION");
+        break;
+    }
+  } else {
+    result = 0.0;
+    console.log("INVALID ARGUMENTS");
   }
   return result.toPrecision(3);
 }
+
+const editorArgOne = document.querySelector(".editor #arg-one");
+const editorOperator = document.querySelector(".editor #operator");
+const editorArgTwo = document.querySelector(".editor #arg-two");
+const editorResult = document.querySelector(".editor #result");
+
+let arguementBuffer = [];
+let currentArgs = editorArgOne;
+
+function enterNumber(number) {
+  arguementBuffer.push(number);
+  currentArgs.textContent = Number.parseFloat(arguementBuffer.join(""));
+}
+
+function enterOperation(operation) {
+
+}
+
+const container = document.querySelector(".container");
+// Query all buttons that can be used as arguments
+const argsButtons = document.querySelectorAll(".args");
+// Query all buttons that can be used as an operator
+const opsButtons = document.querySelectorAll(".ops");
+// Query the equals button (button that solves an entry)
+const equButton = document.querySelector(".equ");
+
+argsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    enterNumber(button.textContent);
+  });
+});
+
